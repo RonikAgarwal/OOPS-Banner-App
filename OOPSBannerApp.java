7c9f4a2d25dd80aa78ebcaf355499ca49640a718
Ronik Agarwal <ronikagarwal@Roniks-MacBook-Air-8.local>
2026-03-03 04:01:17 +0530

    Refactored UC3 using String.join for banner construction

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,57 @@
-public class OOPSBannerApp{
+// UC3: Prints "OOPS" banner using String.join() for better memory efficiency
+
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     *  *    *  *    *  *     ");
-        System.out.println("*     *  *    *  *    *  *     ");
-        System.out.println("*     *  *    *  *****   *****  ");
-        System.out.println("*     *  *    *  *           * ");
-        System.out.println("*     *  *    *  *           * ");
-        System.out.println(" *****   ******  *       ***** ");
-    }
+        String line1 = String.join("   ",
+            " ***** ",
+            " ***** ",
+            " ***** ",
+            " ***** ");
+
+        String line2 = String.join("   ",
+            "*     *",
+            "*     *",
+            "*    * ",
+            "*      ");
+
+        String line3 = String.join("   ",
+            "*     *",
+            "*     *",
+            "*    * ",
+            "*      ");
+
+        String line4 = String.join("   ",
+            "*     *",
+            "*     *",
+            "*****  ",
+            " ***** ");
+
+        String line5 = String.join("   ",
+            "*     *",
+            "*     *",
+            "*      ",
+            "      *");
+
+        String line6 = String.join("   ",
+            "*     *",
+            "*     *",
+            "*      ",
+            "      *");
+
+        String line7 = String.join("   ",
+            " ***** ",
+            " ***** ",
+            "*      ",
+            " ***** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+      }
 }
